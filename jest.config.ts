8603194e219d0cbec8c6e0b3c78c98fd8e86a92a
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-09-21 21:04:15 +0700

    Edited jest config to ESM.

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+import type { JestConfigWithTsJest } from 'ts-jest';
 
-module.exports = {
+const config: JestConfigWithTsJest = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ['<rootDir>/tests'],
+    extensionsToTreatAsEsm: ['.ts'],
+    transform: {
+        '^.+\\.ts$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
+    },
 };
+
+export default config;
